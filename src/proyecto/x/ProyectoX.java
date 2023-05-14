4f85922e03b793cc1402f0219d869e5074cbf1bb
Daniel Reinoso <reinosodaniel86@gmail.com>
2023-05-14 15:04:33 -0500

    provando mi primer repositorio del ciclo while

---
+++ src/proyecto/x/ProyectoX.java
@@ -0,0 +1,15 @@
+
+package proyecto.x;
+
+public class ProyectoX {
+
+    public static void main(String[] args) {
+       int numb=0;
+        do{
+            System.out.println("el numero actual es"+numb);
+            numb++;
+        }while(numb<10);
+        
+    }
+    
+}
